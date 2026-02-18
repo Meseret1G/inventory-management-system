c6db32dbb7e4c988dcc314db7858a407610c4abb
Meseret1G <meseretghebiresilassie@gmail.com>
2026-02-18 22:37:06 +0300

    security: remove superuser creation script

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ pip install -r requirements.txt
 # Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin4321')"
